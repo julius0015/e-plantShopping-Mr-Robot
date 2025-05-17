17f1f97e08bdd6ed962d02a4d40ad6fce2c48768
julius0015 <juliusrabacajr0015@gmail.com>
2025-05-17 13:55:49 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Replace 'your-repo-name' with the exact name of your GitHub repository.
+// If your repository is named 'e-plantShopping-Mr-Robot', use that.
+const repoName = '/e-plantShopping-Mr-Robot/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/e-plantShopping/' : '/',
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? repoName : '/',
   server: {
-    host: true, // Allow external devices to access the app
+    open: true,
   },
-  preview: {
-    host: true, // Allow preview access from LAN
+  build: {
+    sourcemap: true, // Recommended for debugging production builds
   },
-});
+})
